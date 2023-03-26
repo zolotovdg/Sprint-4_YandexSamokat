9768b70137f930ecb9a0b2fa4947ffc5008ad8ef
Dmitry Zolotov <zolotovdg0209@yandex.ru>
2023-03-27 00:03:19 +0300

    Скорректировал замечания по тесту QuestionsTest. Пока разбил на независимые тесты, чтобы успеть в срок. Потом отдельно разберусь с параметризацией.

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @Before
+    public void setChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
